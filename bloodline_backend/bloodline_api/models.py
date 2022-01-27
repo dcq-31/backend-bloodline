8566690a7e7be66cbab3cdbdbabdf1778875c32e
dcq-31 <dariancapoteq@gmail.com>
2022-01-27 00:55:53 -0500

    Improve User and Dogs models

--- bloodline_backend/bloodline_api/models.py
+++ bloodline_backend/bloodline_api/models.py
@@ -4,36 +4,37 @@ from django.db.models.deletion import CASCADE
 
 # User Model
 class User(AbstractUser):
-  created_at = models.DateTimeField(auto_now_add=True)
-  update_at = models.DateTimeField(auto_now=True)
+  created_at = models.DateTimeField('created date', auto_now_add=True)
+  update_at = models.DateTimeField('last update', auto_now=True)
   
   def __str__(self):
     return self.get_full_name()
   
-
-  """
-  Add Fields
-  image
-  """
+"""
+Add Fields
+image
+"""
 # Dog Model
 class Dog(models.Model):
   # Choises of breed (for example)
   AFGHAN_HOUND = 'AF'
   PEKINGESE = 'PE'
   BASSADOR = 'BA'
-  DOGS_BREED = (
+  DOGS_BREED_CHOICHES = (
     (AFGHAN_HOUND, 'Afghan Hound'),
     (PEKINGESE, 'Pekingese'),
     (BASSADOR, 'Bassador'),
   )
   name = models.CharField(max_length=100)
-  code = models.CharField('identification code', max_length=10)
-  user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='related user')
+  code = models.CharField('identification code', max_length=10, unique=True)
+  owner = models.ForeignKey(User, on_delete=models.PROTECT, related_name='dogs', verbose_name='related user', default=1)
   date_birth = models.DateTimeField('date of birthday')
-  breed = models.CharField('animal species', choices=DOGS_BREED, max_length=100)
+  breed = models.CharField('animal species', choices=DOGS_BREED_CHOICHES, max_length=2)
   partners = models.ManyToManyField('self', blank=True, verbose_name='related partners')
-  created_at = models.DateTimeField(auto_now_add=True)
-  update_at = models.DateTimeField(auto_now=True)
+  litters = models.ManyToManyField('self', blank=True, verbose_name='related litters')
+  siblings = models.ManyToManyField('self', blank=True, verbose_name='related siblings')
+  created_at = models.DateTimeField('created date', auto_now_add=True)
+  update_at = models.DateTimeField('last update', auto_now=True)
   
   def __str__(self):
     return self.name + " | " + self.code
