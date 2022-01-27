9f947d3e9ffb14537aac52d25bc51e0e9ebd32ff
dcq-31 <dariancapoteq@gmail.com>
2022-01-27 00:57:24 -0500

    Add drf authentication and user's paths

--- bloodline_backend/bloodline_api/urls.py
+++ bloodline_backend/bloodline_api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import DogList, DogDetail
+from .views import UserDetail, UserList, DogDetail, DogList
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-  path('dogs/', DogList.as_view()),
+  path('users/', UserList.as_view()),
+  path('users/<int:pk>/', UserDetail.as_view()),
+   path('dogs/', DogList.as_view()),
   path('dogs/<int:pk>/', DogDetail.as_view()),
 ]
 
