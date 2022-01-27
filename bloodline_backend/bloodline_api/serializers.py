f08277365d6edb2dd310419e140f36edcd3a153a
dcq-31 <dariancapoteq@gmail.com>
2022-01-27 00:59:33 -0500

    Update fields in serializers and create test http request for dogs

--- bloodline_backend/bloodline_api/serializers.py
+++ bloodline_backend/bloodline_api/serializers.py
@@ -3,12 +3,13 @@ from .models import Dog, User
 
 # User Serializer
 class UserSerializer(serializers.ModelSerializer):
+  dogs = serializers.PrimaryKeyRelatedField(many=True, queryset=Dog.objects.all())
   class Meta:
     model = User
-    fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password']
+    fields = ['id', 'username', 'first_name', 'last_name', 'email','dogs']
 
 # Dog Serializer
 class DogSerializer(serializers.ModelSerializer):
   class Meta:
     model = Dog
-    fields = ['id', 'name', 'code', 'user', 'date_birth', 'breed', 'partners', 'created_at', 'update_at']
+    fields = ['id', 'name', 'code', 'owner', 'date_birth', 'breed', 'partners', 'litters', 'siblings', 'created_at', 'update_at']
